2164e4aa98805494a90d6a0b413b25998f358d92
Dima <78323304+DimaFursov86@users.noreply.github.com>
2021-07-09 05:11:13 +0300

    commit-1

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const galleryItems = [
   {
     preview:
@@ -63,3 +65,64 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const cardsList = document.querySelector(".js-gallery");
+// cardsList.innerHTML = createFotoCardsMarkup(galleryItems);
+cardsList.insertAdjacentHTML("beforeend", createFotoCardsMarkup(galleryItems))
+  function createFotoCardsMarkup(galleryItems) {
+  const markup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+              <a
+                class="gallery__link"
+                href="${original}"
+              >
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                />
+              </a>
+            </li>`;
+  }).join("");
+  return markup;
+};
+
+const modalCloseButton = document.querySelector("[data-action=close-lightbox]")
+const lightBox = document.querySelector(".js-lightbox");
+const fotoImg = document.querySelector(".lightbox__image");
+
+
+cardsList.addEventListener("click", onOpenFotoClik);
+modalCloseButton.addEventListener("click", onCloseFotoClik);
+window.addEventListener("keydown", escapePress)
+
+function onOpenFotoClik(e) {
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  e.preventDefault();
+  lightBox.classList.add("is-open");
+  fotoImg.src = e.target.dataset.source;
+  fotoImg.alt = e.target.alt;
+};
+
+function onCloseFotoClik() {
+  lightBox.classList.remove("is-open");
+  fotoImg.src = "";
+  fotoImg.alt = "";
+}
+
+function escapePress(e) {
+  if (e.code === "Escape") {
+onCloseFotoClik()
+  }
+}
+
+// function arrowPress(e) {
+//    if (e.code === "ArrowLeft") {
+// onOpenFotoClik(e.nextSibling)
+//   }  else if (e.code === "ArrowRight") {
+// onOpenFotoClik(e.previousSibling)
+//     }
+// }
